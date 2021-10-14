db769219f27566d8f40c538ef22781eb3ef23b14
vinicius rocha <vinicius.rocha@dcide.com.br>
2021-10-14 19:29:59 -0300

    Add layout header

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutHomeComponent } from './components/layout-home/layout-home.component';
 import { SharedModule } from "../shared/shared.module";
+import { LayoutHeaderComponent } from './components/layout-header/layout-header.component';
 
 
 @NgModule({
   declarations: [
-    LayoutHomeComponent
+    LayoutHomeComponent,
+    LayoutHeaderComponent
   ],
   imports: [
     SharedModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,10 @@ import { MatTableModule } from "@angular/material/table";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSelectModule } from "@angular/material/select";
 
 @NgModule({
   declarations: [],
@@ -15,7 +19,11 @@ import { MatInputModule } from "@angular/material/input";
     MatPaginatorModule,
     MatFormFieldModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatIconModule,
+    MatSelectModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
